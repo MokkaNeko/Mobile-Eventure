251fc000bfd0df9a1dcdcdc502124c2c6b9ca3f9
Iksanard <iksanardika@gmail.com>
2024-06-03 05:48:11 +0700

    adding login and register

--- app/src/main/java/com/eventurecapstone/eventure/ui/MainActivity.kt
+++ app/src/main/java/com/eventurecapstone/eventure/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.eventurecapstone.eventure
+package com.eventurecapstone.eventure.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.eventurecapstone.eventure.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/eventurecapstone/eventure/ui/WelcomeScreen.kt
+++ app/src/main/java/com/eventurecapstone/eventure/ui/WelcomeScreen.kt
@@ -1,10 +1,11 @@
-package com.eventurecapstone.eventure
+package com.eventurecapstone.eventure.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.eventurecapstone.eventure.R
 
 class WelcomeScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
